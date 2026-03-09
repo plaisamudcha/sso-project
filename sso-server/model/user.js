66e47fb358cb22abc8b68f08c909bd2e3c40cd07
plaisamudcha <plai_samudcha@hotmail.com>
2026-03-09 13:31:26 +0700

    Updated userSchema

--- sso-server/model/user.js
+++ sso-server/model/user.js
@@ -1,13 +1,56 @@
 const mongoose = require("mongoose");
+const crypto = require('crypto');
 
-const userSchema = new mongoose.Schema(
-  {
-    email: { type: String, unique: true },
-    password: String,
+const userSchema = new mongoose.Schema({
+  sub: {
+    type: String,
+    unique: true,
+    default: crypto.randomUUID(),
   },
-  {
-    timestamps: true,
+
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    lowercase: true,
+    index: true,
   },
-);
+
+  emailVerified: {
+    type: Boolean,
+    default: false,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+
+  name: String,
+  givenName: String,
+  familyName: String,
+  picture: String,
+
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
+
+  loginAttempts: {
+    type: Number,
+    default: 0
+  },
+
+  lockUntil: Date,
+
+  lastLoginAt: Date,
+
+  passwordChangedAt: Date,
+}, {
+  timestamps: true,
+});
+
+userSchema.index({ email: 1 });
+userSchema.index({ sub: 1 });
 
 module.exports = mongoose.model("User", userSchema);
